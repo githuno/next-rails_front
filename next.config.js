f25a6c36438637f7fe7d147b801858a4ae26c472
githuno <clas.warehouse@gmail.com>
2023-07-06 05:37:26 +0000

    appディレクトリを使わずに取り組むも失敗

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
     // pagesディレクトリ、pages/index.jsを作成し、以下を追記して旧構成を有効化
     reactStrictMode: true,
-    experimental: {
-        appDir: false,
-    },
+    // experimental: {
+    //     appDir: false,
+    // },
     // ここまで
 }
 
